623c002e98bad432985cd53534b849d9d1678844
Claudiu Belu <cbelu@cloudbasesolutions.com>
2018-06-29 04:56:24 -0700

    Fixes guestbook application
    The guestbook app deploys 3 types of containers: gb-frontend,
    gb-redisslave, and redis. They all had various issues, causing the app
    not to work properly:
    
    1. DNS resolution: gb-frontend and gb-redisslave tries to connect to redis
    and gb-redisslave through DNS name. But if DNS suffix is wrong, they
    won't be able to connect. In the case of Kubernetes, host entries will
    be added for redis-slave and and redis-master for the containers, so
    they will be able to resolve the names.
    
    2. run.ps1 was not being executed.
    
    3. Redis was not being started using the added config value, starting in
    protected mode, refusing all connections.

--- gb-frontend/run.ps1
+++ gb-frontend/run.ps1
@@ -1,2 +1,20 @@
+# there are some issues with DNS name resolution, so we're going to bypass them.
+# guestbook will try to contact redis-master and redis-slave, so we'll insert
+# those entries into the hosts file, if we can.
+if (Test-Path C:\var\run\secrets\kubernetes.io\serviceaccount\namespace) {
+    # we're running inside a kubernetes pod, and we have the namespace.
+    Write-Host "We're running inside a kubernetes pod."
+
+    $namespace = Get-Content C:\var\run\secrets\kubernetes.io\serviceaccount\namespace
+
+    $redisMasterIps = Resolve-DnsName redis-master.$namespace`.svc.cluster.local
+    $ip = $redisMasterIps[0].IPAddress
+    Add-Content -Value "$ip redis-master" -Path C:\Windows\System32\drivers\etc\hosts
+
+    $redisSlaveIps = Resolve-DnsName redis-slave.$namespace`.svc.cluster.local
+    $ip = $redisSlaveIps[0].IPAddress
+    Add-Content -Value "$ip redis-slave" -Path C:\Windows\System32\drivers\etc\hosts
+}
+
 Stop-Process -Name httpd -Force
-& "c:/Users/ContainerAdministrator/AppData/Roaming/Apache24/bin/httpd.exe -t"
+c:/Users/ContainerAdministrator/AppData/Roaming/Apache24/bin/httpd.exe
