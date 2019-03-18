1e7a630820e4f5ca75b34a937bd0b0a60b180615
Adelina Tuvenie <adelina-t@users.noreply.github.com>
2019-03-18 13:45:34 +0200

    Update master extension
    Update the aks-engine master extension to wait for the all pods in kube-system to be running before applying taints to the master.

--- extensions/master_extension/v1/win-e2e-master-extension.sh
+++ extensions/master_extension/v1/win-e2e-master-extension.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Wait for all pods in kube-system namespace to be running before applying taints
+
+OUT=$(kubectl get pods -n kube-system -o custom-columns=STATUS:status.phase | grep -v STATUS | grep -v Running)
+while [[ ! -z $OUT ]]; do 
+sleep 2
+OUT=$(kubectl get pods -n kube-system -o custom-columns=STATUS:status.phase | grep -v STATUS | grep -v Running)
+done
+
 master_node=$(kubectl get nodes | grep master | awk '{print $1}')
 
 kubectl taint nodes $master_node node-role.kubernetes.io/master=:NoSchedule
