46ae318ce14e3d273ea49b83aeb449ffb1614763
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2019-05-07 14:49:29 +0300

    Update agent preprovision extension.

--- extensions/agent_preprovision_extension/v1/node_setup.ps1
+++ extensions/agent_preprovision_extension/v1/node_setup.ps1
@@ -5,4 +5,6 @@ Write-Host "Prepulling all test images"
 Start-BitsTransfer https://raw.githubusercontent.com/kubernetes-sigs/windows-testing/master/images/Utils.ps1
 Start-BitsTransfer https://raw.githubusercontent.com/kubernetes-sigs/windows-testing/master/images/PullImages.ps1
 
-./PullImages.ps1 -Repository "claudiubelu"
+./PullImages.ps1
+
+[System.Environment]::SetEnvironmentVariable('DOCKER_API_VERSION', "1.39", [System.EnvironmentVariableTarget]::Machine)
