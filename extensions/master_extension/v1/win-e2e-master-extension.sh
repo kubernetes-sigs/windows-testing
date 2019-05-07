485b235b9756b8e591c2ef7b579751643c2fd196
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2019-05-07 13:16:25 +0300

    Add template-link.json and template.json to master extension.
    Update master extension to wait for all pods in kube-system to be
    in running state before applying taints.

--- extensions/master_extension/v1/win-e2e-master-extension.sh
+++ extensions/master_extension/v1/win-e2e-master-extension.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Wait for all pods in kube-system namespace to be running before applying taints
+
+OUT=$(kubectl get pods -n kube-system -o custom-columns=STATUS:status.phase | grep -v "STATUS\|Running")
+while [[ ! -z $OUT ]]; do
+sleep 2
+OUT=$(kubectl get pods -n kube-system -o custom-columns=STATUS:status.phase | grep -v "STATUS\|Running")
+echo "Waited 2 seconds." >> /tmp/master_extension.log
+echo $(kubectl get pods --all-namespaces) >> /tmp/master_extension.log
+done
+
 master_node=$(kubectl get nodes | grep master | awk '{print $1}')
 
 kubectl taint nodes $master_node node-role.kubernetes.io/master=:NoSchedule
