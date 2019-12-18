8c6932562320de267f6948f39e5395636fb6a0c5
Yang Lu <yluu@google.com>
2019-12-18 14:27:39 -0800

    Address comments

--- gce/load-test.sh
+++ gce/load-test.sh
@@ -10,7 +10,7 @@ set -o xtrace
 # Taint the Linux nodes to prevent the test workloads from landing on them.
 # TODO: remove this once the issue is resolved:
 # https://github.com/kubernetes/kubernetes/issues/69892
-LINUX_NODES=$(kubectl get nodes -l beta.kubernetes.io/os=linux -o name)
+LINUX_NODES=$(kubectl get nodes -l kubernetes.io/os=linux -o name)
 LINUX_NODE_COUNT=$(echo ${LINUX_NODES} | wc -w)
 for node in $LINUX_NODES; do
   kubectl taint node $node node-under-test=false:NoSchedule
@@ -18,14 +18,12 @@ done
 
 # Untaint the windows nodes to allow test workloads without tolerations to be
 # scheduled onto them.
-WINDOWS_NODES=$(kubectl get nodes -l beta.kubernetes.io/os=windows -o name)
+WINDOWS_NODES=$(kubectl get nodes -l kubernetes.io/os=windows -o name)
 for node in $WINDOWS_NODES; do
   kubectl taint node $node node.kubernetes.io/os:NoSchedule-
 done
 
-# Pre-pull all the test images. The images are currently hard-coded.
-# Eventually, we should get the list directly from
-# https://github.com/kubernetes-sigs/windows-testing/blob/master/images/PullImages.ps1
+# Pre-pull all the test images.
 SCRIPT_ROOT=$(cd `dirname $0` && pwd)
 kubectl create -f ${SCRIPT_ROOT}/loadtest-prepull.yaml
 # Wait for the test images to be pulled onto the nodes.
@@ -33,15 +31,8 @@ sleep ${PREPULL_TIMEOUT:-3m}
 # Check the status of the pods.
 kubectl get pods -o wide
 # Delete the pods anyway since pre-pulling is best-effort
-kubectl delete -f ${SCRIPT_ROOT}/prepull.yaml
+kubectl delete -f ${SCRIPT_ROOT}/loadtest-prepull.yaml
 # Wait a few more minutes for the pod to be cleaned up.
 sleep 1m
 
-# When using customized test command (which we are now), report-dir is not set
-# by default, so set it here.
-# The test framework will not proceed to run tests unless all nodes are ready
-# AND schedulable. Allow not-ready nodes since we make Linux nodes
-# unschedulable.
-# Do not set --disable-log-dump because upstream cannot handle dumping logs
-# from windows nodes yet.
 $GOPATH/src/k8s.io/perf-tests/run-e2e.sh $@
