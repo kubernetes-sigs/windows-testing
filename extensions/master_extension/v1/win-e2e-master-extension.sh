1c7404027b60cd683d76fbbdcc0e2a69568ecfad
Cristian Henzel <CristianHenzel@users.noreply.github.com>
2020-01-03 11:31:18 +0000

    Use daemon set to prepull images

--- extensions/master_extension/v1/win-e2e-master-extension.sh
+++ extensions/master_extension/v1/win-e2e-master-extension.sh
@@ -15,3 +15,14 @@ master_node=$(kubectl get nodes | grep master | awk '{print $1}')
 
 kubectl taint nodes $master_node node-role.kubernetes.io/master=:NoSchedule
 kubectl label nodes $master_node node-role.kubernetes.io/master=NoSchedule
+
+# Prepull images
+kubectl create -f https://raw.githubusercontent.com/kubernetes-sigs/windows-testing/master/gce/prepull.yaml
+# Wait 15 minutes for the test images to be pulled onto the nodes.
+sleep 15m
+# Check the status of the pods.
+kubectl get pods -o wide >> /tmp/master_extension.log
+# Delete the pods anyway since pre-pulling is best-effort
+kubectl delete -f https://raw.githubusercontent.com/kubernetes-sigs/windows-testing/master/gce/prepull.yaml
+# Wait a few more minutes for the pod to be cleaned up.
+sleep 3m
