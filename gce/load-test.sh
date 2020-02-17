0e713fedb6d312e9fd57593833d932df05e2ae15
Yang Lu <yluu@google.com>
2020-02-17 06:57:34 -0800

    Using nodeSelector & Tolerations to prepull test image

--- gce/load-test.sh
+++ gce/load-test.sh
@@ -7,22 +7,6 @@ set -o xtrace
 # When running in prow, the working directory is the root of the test-infra
 # repository.
 
-# Taint the Linux nodes to prevent the test workloads from landing on them.
-# TODO: remove this once the issue is resolved:
-# https://github.com/kubernetes/kubernetes/issues/69892
-LINUX_NODES=$(kubectl get nodes -l kubernetes.io/os=linux -o name)
-LINUX_NODE_COUNT=$(echo ${LINUX_NODES} | wc -w)
-for node in $LINUX_NODES; do
-  kubectl taint node $node node-under-test=false:NoSchedule
-done
-
-# Untaint the windows nodes to allow test workloads without tolerations to be
-# scheduled onto them.
-WINDOWS_NODES=$(kubectl get nodes -l kubernetes.io/os=windows -o name)
-for node in $WINDOWS_NODES; do
-  kubectl taint node $node node.kubernetes.io/os:NoSchedule-
-done
-
 # Pre-pull all the test images.
 SCRIPT_ROOT=$(cd `dirname $0` && pwd)
 kubectl create -f ${SCRIPT_ROOT}/loadtest-prepull.yaml
