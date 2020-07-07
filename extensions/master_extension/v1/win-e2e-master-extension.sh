1747b182fc4bb718ebc161b8348252c19579dd56
Ernest Wong <chuwon@microsoft.com>
2020-07-07 09:22:04 -0700

    Address PR comments

--- extensions/master_extension/v1/win-e2e-master-extension.sh
+++ extensions/master_extension/v1/win-e2e-master-extension.sh
@@ -5,27 +5,20 @@ echo "start master extension" >> /tmp/master_extension.log
 KUBECONFIG="$(find /home/*/.kube/config)"
 KUBECTL="kubectl --kubeconfig=${KUBECONFIG}"
 
-# Wait for coredns pod to be ready
-while true; do
-	COREDNS_READY=$(${KUBECTL} get pods -l k8s-app=kube-dns -n kube-system -o custom-columns=STATUS:status.containerStatuses[0].ready --no-headers)
-	if [ "${COREDNS_READY}" = "true" ]; then
-		echo "$(date -R) - coredns is ready" >> /tmp/master_extension.log
-		${KUBECTL} get pods --all-namespaces >> /tmp/master_extension.log
-		break
-	fi
-	sleep 2
-done
+wait_for_kube_system_pods() {
+    while true; do
+    	NOTREADY_PODS=$(${KUBECTL} get pods -n kube-system -o custom-columns=STATUS:status.containerStatuses[0].ready --no-headers | grep -v "true")
+    	if [ -z "${NOTREADY_PODS}" ]; then
+    		echo "$(date -R) - All kube-system pods are ready" >> /tmp/master_extension.log
+    		${KUBECTL} get pods --all-namespaces >> /tmp/master_extension.log
+    		break
+    	fi
+    	sleep 2
+    done
+}
 
-# Wait for all pods in kube-system namespace to be ready before applying taints
-while true; do
-	NOTREADY_PODS=$(${KUBECTL} get pods -n kube-system -o custom-columns=STATUS:status.containerStatuses[0].ready --no-headers | grep -v "true")
-	if [ -z "${NOTREADY_PODS}" ]; then
-		echo "$(date -R) - All kube-system pods are ready" >> /tmp/master_extension.log
-		${KUBECTL} get pods --all-namespaces >> /tmp/master_extension.log
-		break
-	fi
-	sleep 2
-done
+export -f wait_for_kube_system_pods
+timeout 300 bash -c wait_for_kube_system_pods || exit 1
 
 master_node=$(${KUBECTL} get nodes | grep master | awk '{print $1}')
 
