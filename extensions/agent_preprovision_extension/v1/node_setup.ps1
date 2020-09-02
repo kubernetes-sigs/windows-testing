3f43443cc91c3afcbbe320f37c7c6d0d98b1c7a4
Ernest Wong <chuwon@microsoft.com>
2020-09-02 14:23:28 -0700

    chore: add 1.19 job templates and remove agent_preprovision_extension
    Signed-off-by: Ernest Wong <chuwon@microsoft.com>

--- extensions/agent_preprovision_extension/v1/node_setup.ps1
+++
@@ -1,4 +0,0 @@
-# disable Windows Defender on Windows nodes - workaround for #75148
-Set-MpPreference -DisableRealtimeMonitoring $true
-
-[System.Environment]::SetEnvironmentVariable('DOCKER_API_VERSION', "1.39", [System.EnvironmentVariableTarget]::Machine)
