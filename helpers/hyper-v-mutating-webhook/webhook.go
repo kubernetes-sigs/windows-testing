125d26490a1b7cbcf2e440b4758aac0b60600299
Mark Rossetti <marosset@microsoft.com>
2023-02-22 16:15:41 -0800

    Updating hyperv mutating webhook
    Signed-off-by: Mark Rossetti <marosset@microsoft.com>

--- helpers/hyper-v-mutating-webhook/webhook.go
+++ helpers/hyper-v-mutating-webhook/webhook.go
@@ -1,4 +1,19 @@
-// based on https://github.com/kubernetes-sigs/controller-runtime/tree/master/examples/builtins
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
@@ -6,7 +21,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -14,56 +28,46 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
-// +kubebuilder:webhook:path=/mutate-v1-pod,mutating=true,failurePolicy=fail,groups="",resources=pods,verbs=create;update,versions=v1,name=mpod.kb.io
+// +kubebuilder:webhook:path=/mutate-v1-pod,mutating=true,failurePolicy=fail,groups="",resources=pods,verbs=create;update,versions=v1,name=mpod.kb.io,admissionReviewVersions={v1},sideEffects=None
 
 var (
-	webhookLogger        = ctrl.Log.WithName("webhook")
-	windows2019          = "windows-2019"
-	windows2004          = "windows-2004"
-	windows2004tagSuffix = "-windows-amd64-2004"
+	webhookLogger    = ctrl.Log.WithName("webhook")
+	runtimeClassName = "runhcs-wcow-hypervisor"
 )
 
-// podUpdater updates Pods
 type podUpdater struct {
 	Client  client.Client
 	decoder *admission.Decoder
 }
 
-// podUpdater looks for pods with a nodeSelector kubernetes.io/os=windows and updates them by
-// - adding a runtimeClassName
-// - updating container image fields to make them target single-arch images
-func (a *podUpdater) Handle(ctx context.Context, req admission.Request) admission.Response {
+func (pu *podUpdater) Handle(ctx context.Context, req admission.Request) admission.Response {
 	pod := &corev1.Pod{}
-
-	err := a.decoder.Decode(req, pod)
+	err := pu.decoder.Decode(req, pod)
 	if err != nil {
 		return admission.Errored(http.StatusBadRequest, err)
 	}
 
-	podName := pod.Name
+	mutatePod := true
 
-	// e2e.test does not add nodeSelector fields to pods it schedules so this will
-	// update image fields for ALL pods scheduled to the cluster.
-	webhookLogger.Info(fmt.Sprintf("Adding '%s' runtimeClass to containers in pod '%s'", windows2004, podName))
-	pod.Spec.RuntimeClassName = &windows2004
-
-	containers := []corev1.Container{}
-	for _, c := range pod.Spec.Containers {
-		if !strings.HasSuffix(c.Image, windows2004tagSuffix) {
-			c.Image = c.Image + windows2004tagSuffix
-		}
-		containers = append(containers, c)
+	// Don't apply hyper-v runtime class to hostProcess pods
+	if isHostProcessPod(pod) {
+		mutatePod = false
 	}
-	pod.Spec.Containers = containers
 
-	containers = []corev1.Container{}
-	for _, c := range pod.Spec.InitContainers {
-		if !strings.HasSuffix(c.Image, windows2004tagSuffix) {
-			c.Image = c.Image + windows2004tagSuffix
+	if mutatePod {
+		podName := pod.Name
+		webhookLogger.Info(fmt.Sprintf("Pod %s is being mutated", podName))
+
+		if pod.Annotations == nil {
+			pod.Annotations = make(map[string]string)
 		}
-		containers = append(containers, c)
+
+		pod.Annotations["hyperv-runtimeclass-mutating-webhook"] = "mutated"
+
+		// e2e.test does not add nodeSelector fields to pods it schedules so this will
+		// add the hyperv runtime class to ALL pods scheduled to the cluster.
+		pod.Spec.RuntimeClassName = &runtimeClassName
 	}
-	pod.Spec.InitContainers = containers
 
 	marshaledPod, err := json.Marshal(pod)
 	if err != nil {
@@ -73,11 +77,26 @@ func (a *podUpdater) Handle(ctx context.Context, req admission.Request) admissio
 	return admission.PatchResponseFromRaw(req.Object.Raw, marshaledPod)
 }
 
-// podUpdater implements admission.DecoderInjector.
-// A decoder will be automatically injected.
-
-// InjectDecoder injects the decoder.
-func (a *podUpdater) InjectDecoder(d *admission.Decoder) error {
-	a.decoder = d
+// InjectDecoder injects a decoder into the podUpdater
+func (pu *podUpdater) InjectDecoder(d *admission.Decoder) error {
+	pu.decoder = d
 	return nil
+}
+
+func isHostProcessPod(p *corev1.Pod) bool {
+	// Check if hostProcess is set at pod level
+	if p.Spec.SecurityContext != nil && p.Spec.SecurityContext.WindowsOptions != nil && p.Spec.SecurityContext.WindowsOptions.HostProcess != nil {
+		return *p.Spec.SecurityContext.WindowsOptions.HostProcess
+	}
+
+	// Check if hostProcess is set for any containers
+	if p.Spec.Containers != nil {
+		for _, c := range p.Spec.Containers {
+			if c.SecurityContext != nil && c.SecurityContext.WindowsOptions != nil && c.SecurityContext.WindowsOptions.HostProcess != nil && *c.SecurityContext.WindowsOptions.HostProcess {
+				return true
+			}
+		}
+	}
+
+	return false
 }
