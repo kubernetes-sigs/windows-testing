0158fb8f94a82fdd2701e3dbfbdbc9963cccc6c4
Mark Rossett <marosset@microsoft.com>
2025-10-03 15:21:41 -0700

    fixup

--- scripts/ci-k8s-unit-test.sh
+++ scripts/ci-k8s-unit-test.sh
@@ -41,9 +41,7 @@ echo "Using Go version: ${GO_VERSION}"
 run_remote_cmd ${VM_PUB_IP} ${SSH_KEY_FILE} "c:/prepare_env_windows.ps1 -goVersion ${GO_VERSION}"
 
 echo "Install container features in VM"
-run_remote_cmd ${VM_PUB_IP} ${SSH_KEY_FILE} 'powershell.exe -Command "Install-WindowsFeature -Name Containers -NoRestart"'
-echo "Container feature installed. Initiating restart..."
-run_remote_cmd ${VM_PUB_IP} ${SSH_KEY_FILE} "shutdown.exe /r /f /t 5"
+run_remote_cmd ${VM_PUB_IP} ${SSH_KEY_FILE} "powershell.exe -command { Install-WindowsFeature -Name 'Containers' -Restart }"
 wait_for_vm_restart
 
 # Skip failing tests by default
